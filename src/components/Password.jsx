e4f36885c93e259f9a14c6cd9a2228fa4a15357e
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-09-29 22:53:31 -0400

    Write a bunch of real code for the password!

---
+++ src/components/Password.jsx
@@ -0,0 +1,131 @@
+import { useState } from 'react';
+import './assets/css/Password.css';
+
+const Password = () => {
+	// State variables
+	const [passwordParams, setPasswordParams] = useState({
+		passwordLength: 8,
+		includeUppercase: true,
+		includeLowercase: true,
+		includeNumbers: true,
+		includeSymbols: true,
+		excludeSimilarCharacters: true,
+		includeEveryCharacter: false
+	});
+    const [generatedPassword, setGeneratedPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    // specialCharacters will include these characters: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    // Set a range of Unicode values for the special characters
+    const specialCharacters = Array.from({ length: 30 }, (_, i) => i + 33); // Basic Latin special characters
+
+    // Set a range of Unicode values for the lowercase letters
+    const lowercase = Array.from({ length: 26 }, (_, i) => i + 97); // Basic Latin lowercase letters
+
+    // Set a range of Unicode values for the uppercase letters
+    const uppercase = Array.from({ length: 26 }, (_, i) => i + 65); // Basic Latin uppercase letters
+
+    // Set a range of Unicode values for the numbers
+    const numbers = Array.from({ length: 10 }, (_, i) => i + 48); // Basic Latin numbers
+
+    // Set a range of Unicode values for the similar characters
+    const similarCharacters = [48, 79, 73, 108, 124]; // 0, O, I, l, |
+
+    // Set a range of Unicode values for every character
+    const everyCharacter = Array.from({ length: 1114111 - 33 }, (_, i) => i + 33); // All printable Unicode characters
+
+    // Function to generate a random password
+    const generateRandomPassword = () => {
+        const characterSets = [];
+
+        // Include selected character sets based on options
+        if (passwordParams.includeUppercase) {
+            characterSets.push(uppercase);
+        }
+        if (passwordParams.includeLowercase) {
+            characterSets.push(lowercase);
+        }
+        if (passwordParams.includeNumbers) {
+            characterSets.push(numbers);
+        }
+        if (passwordParams.includeSymbols) {
+            characterSets.push(specialCharacters);
+        }
+        
+        // Merge selected character sets into one array
+        const allCharacters = [].concat(...characterSets);
+
+        let password = '';
+
+        // Generate the password
+        for (let i = 0; i < passwordParams.passwordLength; i++) {
+            const randomIndex = Math.floor(Math.random() * allCharacters.length);
+            password += String.fromCharCode(allCharacters[randomIndex]);
+        }
+
+        setGeneratedPassword(password);
+    };
+
+    // Function to toggle password visibility
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    // Function to copy the generated password to the clipboard
+    const copyPasswordToClipboard = () => {
+        // Your code to copy the password to the clipboard
+    };
+
+    return (
+        <div className="password">
+            <form>
+                <label>
+                    Length
+                    <input type="number" min="1" max="128" id="passwordLength" value={passwordParams.passwordLength} onChange={e => setPasswordParams({ ...passwordParams, passwordLength: e.target.value })} />
+                </label>
+                <label>
+                    Include Uppercase
+                    <input type="checkbox" id="includeUppercase" checked={passwordParams.includeUppercase} onChange={e => setPasswordParams({ ...passwordParams, includeUppercase: e.target.checked })} />
+                </label>
+                <label>
+                    Include Lowercase
+                    <input type="checkbox" id="includeLowercase" checked={passwordParams.includeLowercase} onChange={e => setPasswordParams({ ...passwordParams, includeLowercase: e.target.checked })} />
+                </label>
+                <label>
+                    Include Numbers
+                    <input type="checkbox" id="includeNumbers" checked={passwordParams.includeNumbers} onChange={e => setPasswordParams({ ...passwordParams, includeNumbers: e.target.checked })} />
+                </label>
+                <label>
+                    Include Symbols
+                    <input type="checkbox" id="includeSymbols" checked={passwordParams.includeSymbols} onChange={e => setPasswordParams({ ...passwordParams, includeSymbols: e.target.checked })} />
+                </label>
+                <label>
+                    Exclude Similar Characters
+                    <input type="checkbox" id="excludeSimilarCharacters" checked={passwordParams.excludeSimilarCharacters} onChange={e => setPasswordParams({ ...passwordParams, excludeSimilarCharacters: e.target.checked })} />
+                </label>
+                <label>
+                    Include Every Character (Experimental)
+                    <input type="checkbox" id="includeEveryCharacter" checked={passwordParams.includeEveryCharacter} onChange={e => setPasswordParams({ ...passwordParams, includeEveryCharacter: e.target.checked })} />
+                </label>
+                <label>
+                    Generate Password
+                    <button onClick={generateRandomPassword}>Generate Password</button>
+                </label>
+                <label>
+                    Password
+                    <input type={showPassword ? 'text' : 'password'} value={generatedPassword} readOnly />
+                </label>
+                <label>
+                    Show Password
+                    <input type="checkbox" onChange={togglePasswordVisibility} />
+                </label>
+                <label>
+                    Copy
+                    <button onClick={copyPasswordToClipboard}>Copy</button>
+                </label>
+            </form>
+        </div>
+    );
+};
+
+export default Password;
